6681e16a918c9f4cbcf67cc1a7d841a7fafcb5ff
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-03-01 23:39:13 +0100

    Remove render content and footer

--- src/index.js
+++ src/index.js
@@ -9,8 +9,5 @@ import Footer from './components/Global/Footer';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Header />, document.getElementById('root'));
-ReactDOM.render(<Content />, document.getElementById('root'));
-ReactDOM.render(<Footer />, document.getElementById('root'));
 
 registerServiceWorker();
