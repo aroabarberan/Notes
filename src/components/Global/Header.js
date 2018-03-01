78bc7282cac12d055378ac164b43e752ff4f388f
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-03-01 20:18:04 +0100

    Fixed error import

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -5,9 +5,7 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <header className="Header-header">
           <h1 className="App-title">Welcome to React</h1>
-        </header>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import Header from './components/Global/Header';
+import Content from './components/Global/Content';
+import Footer from './components/Global/Footer';
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Content />, document.getElementById('root'));
+ReactDOM.render(<Footer />, document.getElementById('root'));
 
 registerServiceWorker();
