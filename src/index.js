54b09c1f992b5a3c176080a0b62978e5e409cf17
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-04-07 02:08:43 +0200

    Remove imports without use

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import Header from './components/Global/Header';
-import Content from './components/Global/Content';
-import Footer from './components/Global/Footer';
 
 import registerServiceWorker from './registerServiceWorker';
 
